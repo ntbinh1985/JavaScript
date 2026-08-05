0da7aa7d020e797264176c869e669bd7ee48f8a0
ntbinh1985 <160443342+ntbinh1985@users.noreply.github.com>
2026-08-05 09:19:58 +0700

    Delete Bai 7_xuat_du_lieu/main.js

--- Bai 7_xuat_du_lieu/main.js
+++
@@ -1,7 +0,0 @@
-let a = 4,
-    b =5,
-    kq = a * b;
-console.log("Kết quả: " + a + " nhân với " + b + " bằng: " + kq);
-console.log("Kết quả:",a,"nhân với",b,"bằng:",kq);
-console.log(`Kết quả ${a} nhân với ${b} băng: ${kq}`);
-console.log(`Kết quả %s nhân với %s bằng: `,a,b,kq);
